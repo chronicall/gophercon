159ea97162ef07f3c5b25063a37281c14afc688e
sandra <sandra@greenqloud.com>
2018-05-31 10:10:57 +0000

    Simple http server up

---
+++ cmd/cmd.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+// go run ./cmd/cmd.go
+// curl -i http://127.0.0.1:8000/home
+func main() {
+    log.Printf("Service is starting....")
+
+
+    http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+        log.Printf("Request is processing: %s", r.URL.Path)
+        fmt.Fprint(w,"Hello! Your request was processed.\n")
+    })
+    http.ListenAndServe(":8000", nil)
+}
